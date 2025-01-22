c8def1001a1d1d9be155393fe222821c2c4f54f8
Andy Coates <acoates@marketaxess.com>
2025-01-22 09:44:57 +0000

    Update example to just use config, rather than custom KafkaClientSupplier

--- src/main/java/com/github/big/andy/coates/kafka/App.java
+++ src/main/java/com/github/big/andy/coates/kafka/App.java
@@ -2,7 +2,6 @@ package com.github.big.andy.coates.kafka;
 
 import java.util.Map;
 import java.util.Properties;
-import org.apache.kafka.streams.KafkaClientSupplier;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
@@ -20,13 +19,14 @@ public final class App implements AutoCloseable {
             final Map<String, ?> configOverrides) {
         final Topology topology = new TopologyBuilder().build(sourceTopic, sinkTopic);
 
-        final KafkaClientSupplier clientSupplier =
-                new MultiClusterKafkaClientSupplier(
-                        sourceClusterBootstrap, destinationClusterBootstrap);
-
         this.streams =
                 new KafkaStreams(
-                        topology, properties(applicationId, configOverrides), clientSupplier);
+                        topology,
+                        properties(
+                                applicationId,
+                                configOverrides,
+                                sourceClusterBootstrap,
+                                destinationClusterBootstrap));
     }
 
     public void start() {
@@ -46,10 +46,22 @@ public final class App implements AutoCloseable {
     }
 
     private static Properties properties(
-            final String applicationId, final Map<String, ?> configOverrides) {
+            final String applicationId,
+            final Map<String, ?> configOverrides,
+            final String sourceClusterBootstrap,
+            final String destinationClusterBootstrap) {
         final Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationId);
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "not-used");
+        // Default to destination cluster for producing & restoring
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, destinationClusterBootstrap);
+        // Override where main data is consumed from:
+        props.put(
+                StreamsConfig.MAIN_CONSUMER_PREFIX + StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
+                sourceClusterBootstrap);
+        // Currently, global stores are restored from destination cluster, to switch to source
+        // uncomment:
+        // props.put(StreamsConfig.GLOBAL_CONSUMER_PREFIX + StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
+        // sourceClusterBootstrap);
         props.putAll(configOverrides);
         return props;
     }
